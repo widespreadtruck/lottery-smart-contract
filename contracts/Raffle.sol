99b0dcd31f584d9bcbc1ef2a30dccd4a5b1dc890
Nikita Guliaev <gulnikita63@gmail.com>
2022-12-30 16:47:55 +0300

    add events

---
+++ contracts/Raffle.sol
@@ -0,0 +1,86 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AutomationCompatibleInterface.sol";
+import "hardhat/console.sol";
+
+error Raffle__NotEnoughETHEntered();
+
+contract Raffle is VRFConsumerBaseV2 {
+  /* State Variables */
+  uint256 private immutable i_entranceFee;
+  address payable[] private s_players;
+  VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+  bytes32 private immutable i_gasLane;
+  uint64 private immutable i_subscriptionId;
+  uint32 private immutable i_callbackGasLimit;
+  uint16 private constant REQUEST_CONFIRMATIONS = 3;
+  uint16 private constant NUM_WORDS = 1;
+
+  /* Events */
+  event RaffleEnter(address indexed player);
+  event RequestedRaffleWinner(uint256 indexed requestedId);
+
+  constructor(
+    address vrfCoordinatorV2,
+    uint256 entranceFee,
+    bytes32 gasLane,
+    uint64 subscriptionId,
+    uint32 callbackGasLimit
+  ) VRFConsumerBaseV2(vrfCoordinatorV2) {
+    i_entranceFee = entranceFee;
+    i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+    i_gasLane = gasLane;
+    i_subscriptionId = subscriptionId;
+    i_callbackGasLimit = callbackGasLimit;
+  }
+
+  //enter lottery
+  function enterRaffle() public payable {
+    if (msg.value < i_entranceFee) {
+      revert Raffle__NotEnoughETHEntered();
+    }
+    // msg.sender can't be used on its own here,
+    // we need to type cast it
+    s_players.push(payable(msg.sender));
+    // events
+    emit RaffleEnter(msg.sender);
+  }
+
+  //pick winner
+  function requestRandomWinner() external {
+    // request random num
+    // 2 trx process
+    // once we get it, do smth with it
+    uint256 requestId = i_vrfCoordinator.requestRandomWords(
+      i_gasLane,
+      i_subscriptionId,
+      REQUEST_CONFIRMATIONS,
+      i_callbackGasLimit,
+      NUM_WORDS
+    );
+    emit RequestedRaffleWinner(requestId);
+  }
+
+  function fulfillRandomWords(
+    uint256 requestId,
+    uint256[] memory randomWords
+  ) internal override {
+    
+  }
+
+  function getEntranceFee() public view returns (uint256) {
+    return i_entranceFee;
+  }
+
+  function getPlayer(uint256 index) public view returns (address) {
+    return s_players[index];
+  }
+}
+
+//winner to be selected every X mins
+
+//Chainlink Oracle, auto execution
