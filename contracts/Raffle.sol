1420b5f14a85f8e9928c88069823607899e4e790
Nikita Guliaev <gulnikita63@gmail.com>
2023-01-03 18:22:47 +0300

    add helper-config, mocks, tests

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -9,8 +9,27 @@ import "hardhat/console.sol";
 
 error Raffle__NotEnoughETHEntered();
 error Raffle__TransferFailed();
+error Raffle__NotOpen();
+error Raffle__UpkeepNotNeeded(
+  uint256 currentBalance,
+  uint256 numPlayers,
+  uint256 raffleState
+);
+
+/**
+ * @title Lottery contract
+ * @author Nikita Guliaev
+ * @notice This contract creates a decentralized Lottery smart contract
+ * @dev It implements Chainlink VRF v2 & Chainlink Keepers
+ */
 
 contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
+  /* Type Declarations*/
+  enum RaffleState {
+    OPEN,
+    CALCULATING
+  } // uint256 0 = OPEN, 1 = CALCULATING
+
   /* State Variables */
   uint256 private immutable i_entranceFee;
   address payable[] private s_players;
@@ -23,30 +42,42 @@ contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
 
   /* Lottery Variables */
   address private s_recentWinner;
+  RaffleState private s_raffleState;
+  uint256 private s_lastTimeStamp;
+  uint256 private immutable i_interval;
 
   /* Events */
   event RaffleEnter(address indexed player);
   event RequestedRaffleWinner(uint256 indexed requestedId);
   event WinnerPicked(address indexed winner);
 
+  /* Functions */
+  // args go to the ARGS array when we deploy
   constructor(
-    address vrfCoordinatorV2,
+    address vrfCoordinatorV2, //contract address
     uint256 entranceFee,
     bytes32 gasLane,
     uint64 subscriptionId,
-    uint32 callbackGasLimit
+    uint32 callbackGasLimit,
+    uint256 interval
   ) VRFConsumerBaseV2(vrfCoordinatorV2) {
     i_entranceFee = entranceFee;
     i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
     i_gasLane = gasLane;
     i_subscriptionId = subscriptionId;
     i_callbackGasLimit = callbackGasLimit;
+    s_raffleState = RaffleState.OPEN;
+    s_lastTimeStamp = block.timestamp;
+    i_interval = interval;
   }
 
   //enter lottery
   function enterRaffle() public payable {
     if (msg.value < i_entranceFee) {
       revert Raffle__NotEnoughETHEntered();
+    }
+    if (s_raffleState != RaffleState.OPEN) {
+      revert Raffle__NotOpen();
     }
     // msg.sender can't be used on its own here,
     // we need to type cast it
@@ -56,22 +87,42 @@ contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
   }
 
   /**
-* @dev This is the function that the Chainlink Keeper nodes call
-     * they look for `upkeepNeeded` to return True.
-     * the following should be true for this to return true:
-     * 1. The time interval has passed between raffle runs.
-     * 2. The lottery is open.
-     * 3. The contract has ETH.
-     * 4. Implicity, subscription is funded with LINK.   */
-  function checkUpkeep(bytes calldata /* checkData */) external override {
-    
+   * @dev This is the function that the Chainlink Keeper nodes call
+   * they look for `upkeepNeeded` to return True.
+   * the following should be true for this to return true:
+   * 1. The time interval has passed between raffle runs.
+   * 2. The lottery is open.
+   * 3. The contract has ETH.
+   * 4. Implicity, subscription is funded with LINK.   */
+  function checkUpkeep(
+    bytes memory /* checkData */
+  )
+    public
+    override
+    returns (bool upkeepNeeded, bytes memory /* performData */)
+  {
+    bool isOpen = (RaffleState.OPEN == s_raffleState);
+    //winner to be selected every X mins
+    bool timePassed = (block.timestamp - s_lastTimeStamp) > i_interval;
+    bool hasPlayers = (s_players.length > 0);
+    bool balance = address(this).balance > 0;
+    upkeepNeeded = (isOpen && timePassed && hasPlayers && balance);
   }
 
   //pick winner
-  function requestRandomWinner() external {
+  function performUpkeep(bytes calldata /* performData */) external override {
+    (bool upKeepNeeded, ) = checkUpkeep("");
+    if (!upKeepNeeded) {
+      revert Raffle__UpkeepNotNeeded(
+        address(this).balance,
+        s_players.length,
+        uint256(s_raffleState)
+      );
+    }
     // request random num
     // 2 trx process
     // once we get it, do smth with it
+    s_raffleState = RaffleState.CALCULATING;
     uint256 requestId = i_vrfCoordinator.requestRandomWords(
       i_gasLane,
       i_subscriptionId,
@@ -89,9 +140,12 @@ contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
     uint256 indexOfWinner = randomWords[0] % s_players.length;
     address payable recentWinner = s_players[indexOfWinner];
     s_recentWinner = recentWinner;
-    (bool success, ) = recentWinner.call{value: address(this).balance}('');
+    s_raffleState = RaffleState.OPEN;
+    s_lastTimeStamp = block.timestamp;
+    s_players = new address payable[](0);
+    (bool success, ) = recentWinner.call{value: address(this).balance}("");
     if (!success) {
-        revert Raffle__TransferFailed();
+      revert Raffle__TransferFailed();
     }
     emit WinnerPicked(recentWinner);
   }
@@ -104,11 +158,31 @@ contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
     return s_players[index];
   }
 
-  function getRecentWinner() public view returns(address) {
+  function getRecentWinner() public view returns (address) {
     return s_recentWinner;
   }
+
+  function getRaffleState() public view returns (RaffleState) {
+    return s_raffleState;
+  }
+
+  function getNumWords() public pure returns (uint256) {
+    return NUM_WORDS;
+  }
+
+  function getNumberOfPlayers() public view returns (uint256) {
+    return s_players.length;
+  }
+
+  function getLastTimeStamp() public view returns (uint256) {
+    return s_lastTimeStamp;
+  }
+
+  function getRequestConfirmations() public pure returns (uint256) {
+    return REQUEST_CONFIRMATIONS;
+  }
+
+  function getInterval() public view returns (uint256) {
+    return i_interval;
+  }
 }
-
-//winner to be selected every X mins
-
-//Chainlink Oracle, auto execution
--- hardhat.config.js
+++ hardhat.config.js
@@ -9,4 +9,25 @@ require("dotenv").config()
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    player: {
+      default: 1,
+    },
+  },
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      blockConfirmations: 1,
+    },
+    goerli: {
+      chainId: 5,
+      blockConfirmations: 6,
+      url: process.env.GOERLI_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 }
